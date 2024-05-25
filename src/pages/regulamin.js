b684b1e9aa845378c303707d3916336fd9eb1f0f
Paweł Mroczka <pawel.mroczka@neti-soft.com>
2024-05-25 21:46:11 +0200

    change regulamin

--- src/pages/regulamin.js
+++ src/pages/regulamin.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
-import reg1 from '../assets/reg1.jpg'
-import reg2 from '../assets/reg2.jpg'
-import reg3 from '../assets/reg3.jpg'
+import reg1 from '../assets/reg1.png'
+import reg2 from '../assets/reg2.png'
+import reg3 from '../assets/reg3.png'
+import reg4 from '../assets/reg4.png'
+import reg5 from '../assets/reg5.png'
 
 const ImageBox = styled(Box)(({ theme }) => ({
   maxWidth: 1100,
@@ -22,6 +24,8 @@ const Regulamin = () => {
         <img src={reg1} alt="Image 1" />
         <img src={reg2} alt="Image 2" />
         <img src={reg3} alt="Image 3" />
+        <img src={reg4} alt="Image 4" />
+        <img src={reg5} alt="Image 4" />
       </ImageBox>
     </div>
   );
